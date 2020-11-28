7b3bf9558857695c11cf889c8cb365160b733ea3
宜之（武隆隆） <740971721@qq.com>
2020-11-28 10:29:59 +0800

    first commit

---
+++ lib/flutter_app_upgrade.dart
@@ -0,0 +1,10 @@
+
+
+library flutter_app_upgrade;
+
+export 'src/app_upgrade.dart';
+export 'src/flutter_upgrade.dart';
+export 'src/app_market.dart';
+export 'src/download_status.dart';
+export 'src/simple_app_upgrade.dart';
+export 'src/liquid_progress_indicator.dart';
